d13fb3187f2993e7cafc0a22f750f9f21851708a
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-02-19 21:11:12 +0000

    First pass.

--- src/visualisation/Wip.tsx
+++ src/visualisation/Wip.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useRef, useState } from 'react';
+import * as d3 from 'd3';
+
 export default function Wip() {
-  return <Circle />
+  return <Circle />;
 }
 
 function Circle() {
-  return <h1>tert</h1>
+  const [dataset, setDataset] = useState(generateDataset());
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const svgElement = d3.select(ref.current);
+
+    svgElement
+      .selectAll('circle')
+      .data(dataset)
+      .join('circle')
+      .attr('cx', (d) => d[0])
+      .attr('cy', (d) => d[1])
+      .attr('r', 3);
+
+    setInterval(() => {
+      const newDataset = generateDataset();
+      setDataset(newDataset);
+    }, 2000);
+  }, [dataset]);
+
+  return <svg viewBox="0 0 100 50" ref={ref} />;
+}
+
+function generateDataset() {
+  return Array(10)
+    .fill(0)
+    .map(() => [Math.random() * 80 + 10, Math.random() * 35 + 10]);
 }
