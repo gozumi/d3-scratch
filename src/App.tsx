23f32e1335438e06ba8210d5f38044f44d824e24
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-02-19 21:26:25 +0000

    Second pass.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
-import Wip from './visualisation/Wip';
+import './App.css'
+import Wip from './visualisation/Wip'
 
 function App() {
   return (
-    <div>
+    <>
       <Wip />
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/visualisation/Wip.tsx
+++ src/visualisation/Wip.tsx
@@ -1,37 +1,30 @@
-import { useEffect, useRef, useState } from 'react';
-import * as d3 from 'd3';
+import { useEffect, useState } from 'react'
 
 export default function Wip() {
-  return <Circle />;
+  return <Circle />
 }
 
 function Circle() {
-  const [dataset, setDataset] = useState(generateDataset());
-
-  const ref = useRef();
+  const [dataset, setDataset] = useState(generateDataset())
 
   useEffect(() => {
-    const svgElement = d3.select(ref.current);
-
-    svgElement
-      .selectAll('circle')
-      .data(dataset)
-      .join('circle')
-      .attr('cx', (d) => d[0])
-      .attr('cy', (d) => d[1])
-      .attr('r', 3);
-
     setInterval(() => {
-      const newDataset = generateDataset();
-      setDataset(newDataset);
-    }, 2000);
-  }, [dataset]);
+      const newDataset = generateDataset()
+      setDataset(newDataset)
+    }, 2000)
+  }, [dataset])
 
-  return <svg viewBox="0 0 100 50" ref={ref} />;
+  return (
+    <svg viewBox="0  0 100 50">
+      {dataset.map(([x, y]) => (
+        <circle cx={x} cy={y} r="3" />
+      ))}
+    </svg>
+  )
 }
 
 function generateDataset() {
   return Array(10)
     .fill(0)
-    .map(() => [Math.random() * 80 + 10, Math.random() * 35 + 10]);
+    .map(() => [Math.random() * 80 + 10, Math.random() * 35 + 10])
 }
